014cd52efd0c7c9efe14831fc4715975e409bf35
Ervis Trupja <ervistrupja@outlook.com>
2021-04-20 13:26:33 +0200

    added ConfigureBuiltInExceptionHandler

---
+++ School.API/Exceptions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using School.API.Data.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace School.API.Exceptions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+
+        //Built-int Exception Handler
+        public static void ConfigureBuiltInExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    var contextRequest = context.Features.Get<IHttpRequestFeature>();
+
+                    if (contextFeature != null)
+                    {
+                        var errorString = new ErrorResponseData()
+                        {
+                            StatusCode = (int)HttpStatusCode.InternalServerError,
+                            Message = contextFeature.Error.Message,
+                            Path = contextRequest.Path
+                        }.ToString();
+
+                        await context.Response.WriteAsync(errorString);
+                    }
+                });
+            });
+        }
+    }
+}
