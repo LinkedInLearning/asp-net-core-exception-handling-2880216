bd2d44e56ac4731726ffc552bb2de63458da1566
Ervis Trupja <ervistrupja@outlook.com>
2021-04-20 13:11:41 +0200

    added error-response-data

---
+++ School.API/Data/Responses/ErrorResponseData.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School.API.Data.Responses
+{
+    public class ErrorResponseData
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string Path { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
